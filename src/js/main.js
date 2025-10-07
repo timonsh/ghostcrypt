ced9747e69b41d18f9d6c44ef24a9bc82f8a634b
= <=>
2025-10-07 11:59:41 +0200

    File Upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,54 @@
 "use strict";
+
+// Upload File
+
+let uploaded_file = null;
+let crypt_action = null;
+
+function upload_file() {
+
+    let create_file_input = document.createElement('input');
+    create_file_input.type = 'file';
+
+    create_file_input.onchange = function (upload) {
+
+        const file = upload.target.files[0];
+
+        if (file) {
+
+            const reader = new FileReader();
+
+            reader.onload = function (event) {
+
+                uploaded_file = {
+                    name: file.name,
+                    type: file.type,
+                    size: file.size,
+                    lastModified: file.lastModified,
+                    content: event.target.result
+                };
+
+                process_file();
+
+            };
+
+            reader.readAsDataURL(file);
+
+        }
+    }
+
+    create_file_input.click();
+
+}
+
+// Process File
+
+function process_file() {
+
+    if (uploaded_file.name.endsWith('.ghost')) {
+        crypt_action = 'decrypt';
+    } else {
+        crypt_action = 'encrypt';
+    }
+
+}
